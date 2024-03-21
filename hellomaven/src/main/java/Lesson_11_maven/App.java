f899c1aa3a9721ff4756947c61fab8b30dde30be
Natalia Brukovskaya <brukovskaanatala@gmail.com>
2024-03-21 16:24:22 +0300

    Lesson_11_maven

---
+++ hellomaven/src/main/java/Lesson_11_maven/App.java
@@ -0,0 +1,13 @@
+package Lesson_11_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
